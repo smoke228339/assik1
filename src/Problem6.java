984f38c8a8257016beef3532e7952d9b645bf542
Sultan <sultanabayev1339gmail.com>
2025-03-31 02:31:37 +0500

    6

---
+++ src/Problem6.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Problem6 {
+    public static int power(int a, int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return a * power(a, n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.println(a + "^" + n + " = " + power(a, n));
+    }
+}
